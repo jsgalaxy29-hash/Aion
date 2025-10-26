44874365678321f8b70447c66c72a8bc29c3f4b9
jsgalaxy29-hash <jsgalaxy29@gmail.com>
2025-10-26 22:50:11 +0100

    1.4

---
+++ Aion.AppHost/Services/HttpContextUserContext.cs
@@ -0,0 +1,49 @@
+﻿using Aion.DataEngine.Interfaces;
+using Aion.Security.Extensions;
+using Microsoft.AspNetCore.Http;
+
+namespace Aion.AppHost.Services
+{
+    /// <summary>
+    /// Implémentation de IUserContext qui récupère les informations
+    /// utilisateur depuis HttpContext (claims).
+    /// </summary>
+    public class HttpContextUserContext : IUserContext
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HttpContextUserContext(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public int UserId
+        {
+            get
+            {
+                var user = _httpContextAccessor.HttpContext?.User;
+                return (int)(user.GetUserId());
+            }
+        }
+
+        public int TenantId
+        {
+            get
+            {
+                var user = _httpContextAccessor.HttpContext?.User;
+                return (int)user.GetTenantId();
+            }
+        }
+
+        public string? Username
+        {
+            get
+            {
+                var user = _httpContextAccessor.HttpContext?.User;
+                return user?.Identity?.Name;
+            }
+        }
+
+       
+    }
+}
