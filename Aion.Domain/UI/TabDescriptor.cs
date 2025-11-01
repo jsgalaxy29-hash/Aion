2f2f0cbc5c9057bd774ad5b3f37299670e30964f
jsgalaxy29-hash <jsgalaxy29@gmail.com>
2025-11-01 19:38:02 +0100

    2.7

--- Aion.Domain/UI/TabDescriptor.cs
+++ Aion.Domain/UI/TabDescriptor.cs
@@ -6,5 +6,23 @@ namespace Aion.Domain.UI
     /// <summary>
     /// Modèle léger représentant un onglet ouvert dans l'interface. Conserve l'identifiant, le titre, la route et les paramètres éventuels.
     /// </summary>
-    public sealed record TabDescriptor(Guid Id, string Title, string Route, IDictionary<string, object?>? Parameters, bool IsDirty);
+    public sealed class TabDescriptor
+    {
+
+        public TabDescriptor(Guid id, string title, string route, IDictionary<string, object?>? parameters,  bool isDirty)
+        {
+            Id = id;
+            Title = title;
+            Route = route;
+            Parameters = parameters ?? new Dictionary<string, object?>();
+            IsDirty = isDirty;
+            IsActive = false;
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Route { get; set; }
+        public IDictionary<string, object?> Parameters { get; set; }
+        public bool IsDirty { get; set; }
+        public bool IsActive { get; set; }
+    }
 }
